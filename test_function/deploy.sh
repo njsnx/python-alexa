82b5a77484b1d88611cc1d23a07cecabd50b5e3e
Neil Stewart <nmyster@gmail.com>
2016-11-20 18:09:31 -0500

    more moving

--- test_function/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp ../alexa/* ./alexa/
-zip -r function.zip alexa/* main.py
-aws s3 cp function.zip s3://njsnet-deployments/lambda/Alexa/alexa-lib/ --profile njsnet-sceptre
-
-aws lambda update-function-code --s3-key "lambda/Alexa/alexa-lib/function.zip" --s3-bucket njsnet-deployments --function-name lambda_skill_test --profile njsnet-sceptre --region eu-west-1
-
